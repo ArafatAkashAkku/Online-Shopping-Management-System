06b51e4e34c821366df63c9f5bff1907d0df918f
FerdousReza1998 <ferdous.niloy.bd@gmail.com>
2023-05-20 23:08:56 +0600

    niloy worked on catagory and subcatagory part

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -1,27 +1,35 @@
 <div class="span3">
-	<div class="sidebar">
+					<div class="sidebar">
 
 
-		<ul class="widget widget-menu unstyled">
+<ul class="widget widget-menu unstyled">
+			
+							
+							<li>
+								<a href="manage-users.php">
+									<i class="menu-icon icon-group"></i>
+									Manage users
+								</a>
+							</li>
+						</ul>
 
-				<li>
-					<a href="manage-users.php">
-						<i class="menu-icon icon-group"></i>
-						Manage users
-					</a>
-				</li>
-			</ul>
 
-			<ul class="widget widget-menu unstyled">
-				<li><a href="user-logs.php"><i class="menu-icon icon-tasks"></i>User Login Log </a></li>
+						<ul class="widget widget-menu unstyled">
+                                <li><a href="category.php"><i class="menu-icon icon-tasks"></i> Create Category </a></li>
+                                <li><a href="subcategory.php"><i class="menu-icon icon-tasks"></i>Sub Category </a></li>
+                        
+                            </ul><!--/.widget-nav-->
 
-				<li>
-					<a href="logout.php">
-						<i class="menu-icon icon-signout"></i>
-						Logout
-					</a>
-				</li>
-			</ul>
+						<ul class="widget widget-menu unstyled">
+							<li><a href="user-logs.php"><i class="menu-icon icon-tasks"></i>User Login Log </a></li>
+							
+							<li>
+								<a href="logout.php">
+									<i class="menu-icon icon-signout"></i>
+									Logout
+								</a>
+							</li>
+						</ul>
 
-	</div><!--/.sidebar-->
-</div><!--/.span3-->
+					</div><!--/.sidebar-->
+				</div><!--/.span3-->
